cd8c95fc4301b2268c8f7c7ad89c3d8965e69387
Tanner Disney <disneytanner@gmail.com>
2020-07-18 15:32:06 -0700

    Completed Small Usage of NewsAPI

---
+++ ApiTestingGrounds/Controllers/APIController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NewsAPI;
+using NewsAPI.Models;
+using NewsAPI.Constants;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTestingGrounds.Controllers
+{
+    public class APIController : Controller
+    {
+        public IActionResult NewsAPI()
+        {
+            return View();
+        }
+    }
+}
